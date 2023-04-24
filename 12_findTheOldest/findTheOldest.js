ccd4b22a2ab0aadbeb73d7b8b43f9ccacf61b937
t0nik <aqwrolds202@gmail.com>
2023-04-25 00:32:53 +0200

    Solve 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+  const sorted = people.sort((a, b) => {
+    const ageA = (a.yearOfDeath ? a.yearOfDeath : new Date().getFullYear()) - a.yearOfBirth;
+    const ageB = (b.yearOfDeath ? b.yearOfDeath : new Date().getFullYear()) - b.yearOfBirth;
+    return ageA > ageB ? -1 : 1;
+  });
+  // console.table(sorted);
+  return sorted[0];
 };
 
 // Do not edit below this line
