0a1988a912c8b4f7ecebdecac6abb672c1721009
t0nik <aqwrolds202@gmail.com>
2023-04-24 22:15:32 +0200

    Solve 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+	return numbers.reduce((total, number) => add(total, number), 0)
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+	return numbers.reduce((total, number) => total * number, 1)
 };
 
-const power = function() {
-	
+const power = function(n,pow) {
+	return n ** pow;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	if (n === 0 ) {
+    return 1;
+  } else {
+    return n * factorial(n-1);
+  }
 };
 
 // Do not edit below this line
