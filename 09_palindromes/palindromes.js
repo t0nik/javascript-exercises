cceecd74f6cb84197c6be31e5853f5aab1163071
t0nik <aqwrolds202@gmail.com>
2023-04-24 23:20:51 +0200

    Solve 09_palindromes.js

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (text) {
+  let textReversed = "";
+  text = text.replace(/[^a-zA-Z0-9]/g, ''); // Remove special characters and whitespaces
+  text = text.toLowerCase(); // Upper case and lower case equality
+  for (char of text) {
+    textReversed = char + textReversed;
+  }
+  return text === textReversed;
 };
 
 // Do not edit below this line
