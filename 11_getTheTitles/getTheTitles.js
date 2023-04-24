90b461235ffbe36b0e1d2f4393fa028c695a0234
t0nik <aqwrolds202@gmail.com>
2023-04-25 00:07:27 +0200

    Solve 11_getTheTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,11 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+  const titles = books.reduce((arr, book) => {
+    arr.push(book.title);
+    return arr;
+  }, []);
+  return titles;
 };
+// map() instead of reduce()
 
 // Do not edit below this line
 module.exports = getTheTitles;
